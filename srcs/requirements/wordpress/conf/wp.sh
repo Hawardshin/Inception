55210e89cac5acdd387b89180bf79e69f73fe430
joushin42 <wnddms12345@naver.com>
2023-06-24 18:35:19 +0900

    Edit makefile and comment edit
    this is for dockerfile edit and dir delete in
    makefile dir data volume

--- srcs/requirements/wordpress/conf/wp.sh
+++ srcs/requirements/wordpress/conf/wp.sh
@@ -3,14 +3,14 @@
 if [ ! -f "/var/www/html/index.php" ]; then
 # install wordpress core file
   wp core download
-#wp-config.php is wordpress config file 
+#wp-config.php is wordpress config file
 # dbname, username, passwd set and mariadb connection
   wp config create --dbhost=mariadb:3306 --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS}
 # wordpress init config for website and root user
   wp core install --url=$WP_URL --title=$WP_TITLE \
                 --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS \
                 --admin_email=$WP_ADMIN_MAIL --skip-email
-# wordpress 
+# wordpress
   wp user create $WP_USER_NAME $WP_USER_MAIL --user_pass=$WP_USER_PASS
 fi
 
